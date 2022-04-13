4f8838a49e09584ce6b404df3f3c3dcdf6952166
Wesleycnc <wesleycaldas728@gmail.com>
2022-04-13 14:18:23 -0300

    Update 06-funcoes.js
    function e arrow

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -25,11 +25,22 @@ console.log(`Ano: ${ano}`);
 
 
                 //parametros/argumentos
-function soma(valor1, valor2 ){
+    // VERSÃO 1: sintaxe tradicional
+/* function soma(valor1, valor2 ){
     let total = valor1 + valor2;
     return total;
 
-}
+} */
+
+// VERSÃO 2: sintaxe ARROW FUNCTION (Função SETA/FLECHA)
+const soma = (valor1, valor2) => {
+    let total = valor1 + valor2;
+    return total;
+
+}; 
+
+// Arrow Function com retorno IMPLICITO
+const soma1 = (valor1, valor2) => valor1 + valor2; 
 
 console.log( soma(10, 5) );
 // console.log(total); // erro, pois é variavel local
@@ -40,3 +51,46 @@ console.log(resultado);
 if(resultado <1000){
     console.log("Abaixo da meta");
 }
+
+
+
+linha();
+// versão1: tradicional
+/* function dobra(numero){
+    let resultado = numero * 2;
+    return resultado;
+} */
+
+/* function dobra(numero){
+    return numero * 2;
+} */
+
+// Versão 2: arrow function
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado
+
+}; */
+
+/* const dobra = (numero) => {
+    return numero * 2;
+}; */
+
+// retorno IMPLICITO (remove {} e return)
+// Obs.: como só temos um parametro (numero), pode tirar os ()
+const dobra = numero => numero * 2;
+
+console.log( dobra(10) );
+console.log( dobra(150) );
+
+
+linha();
+
+let nota1 = 8.5;
+let nota2 = 7.4;
+
+const calculaMedia = (n1, n2) => (n1 + n2) /2;
+console.log( calculaMedia (nota1, nota2 ) );
+
+//obs.: se não tem parâmetro, tambem precisa usar ()
+    
